e36117025b1d1d6198ae951ab9114da53914898c
ArtomDovgal <artomdovgalnew@gmail.com>
2023-11-26 21:05:15 +0200

    commit #1

--- app/src/main/java/com/example/lab4/network/LibraryApi.java
+++ app/src/main/java/com/example/lab4/network/LibraryApi.java
@@ -10,16 +10,11 @@ import retrofit2.http.Query;
 
 public interface LibraryApi {
 
-//    @GET("/search.json")
-//    Call<List<BookNetworkEntity>> getBooks(@Query("title")String author,@Query("limit")String limit);
-    //Call<List<BookNetworkEntity>> getBooks(@Query("author")String author,@Query("limit")String limit);
-
     @GET("/search.json")
     Call<JsonObject> getBooks(@Query("title")String title,@Query("author") String author,
                               @Query("limit") Integer limit);
 
-//    @GET("/{key}.json")
-//    Call<JsonObject> getBookByKey(@Path("key")String key);
+
     @GET("/search.json")
     Call<JsonObject> getBookByKey(@Query("q")String key);
 
